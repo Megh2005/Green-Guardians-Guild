4059367890afbce1105bf3a5d5f9b7386c05e48c
Megh Deb <147889330+Megh2005@users.noreply.github.com>
2024-04-20 21:29:21 +0530

    GGG

---
+++ script.js
@@ -0,0 +1,47 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a [href*' + id + ']').classList.add('active')
+            })
+        }
+    })
+}
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxdPWuA-CenlhV7TvNsEH9E_atzP5L_xaj5M8j_tJeE0mDbPbnvcymgb-6ZJPycQg/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            msg.innerHTML = "Your Enquiry Has Been Listed"
+            setTimeout(function () {
+                msg.innerHTML = ""
+            }, 3000)
+            form.reset()
+        })
+        .catch(error => console.error('Error!', error.message))
+})
+
+
+// document.addEventListener("contextmenu", function (x) {
+//     x.preventDefault()
+// }, false);
